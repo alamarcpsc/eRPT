b83fa14f176eeb9ac4e275573149da8e87c98c18
alamarcpsc <alamarcpsc@gmail.com>
2016-02-29 20:53:35 -0800

    Update dbconnection.js

--- sql/dbconnection.js
+++ sql/dbconnection.js
@@ -9,13 +9,13 @@ var dbConfig = {
 };
 
 module.exports = {
-    getEMP: function(req, res, next){
+    getBinders: function(req, res, next){
         var conn = new sql.Connection(dbConfig);
         var req = new sql.Request(conn);
 
         conn.connect(function (err) {
             if (err) {
-                console.log(err);
+                console.log("Error: couldn't connect to the db");
                 return next();
             }
             req.query("SELECT * from dbo.Binder", function (err, recordset) {
@@ -24,6 +24,30 @@ module.exports = {
                     return next();
                 }
                 else {
+                    res.recordset = recordset;
+                    return next();
+                }
+                conn.close();
+                return next();
+            });
+        });
+    },
+    getNames: function(req, res, next){
+        var conn = new sql.Connection(dbConfig);
+        var req = new sql.Request(conn);
+
+        conn.connect(function (err) {
+            if (err) {
+                console.log("Error: couldn't connect to the db");
+                return next();
+            }
+            req.query("SELECT * from dbo", function (err, recordset) {
+                if (err) {
+                    console.log(err);
+                    return next();
+                }
+                else {
+                    console.log(recordset);
                     res.recordset = recordset;
                     return next();
                 }
