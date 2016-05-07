24dc86d4fb533e0bd0882d0f151532a9f701ad53
alamarcpsc <alamarcpsc@gmail.com>
2016-05-06 20:13:40 -0700

    Added files via upload

--- config/passport.js
+++ config/passport.js
@@ -3,7 +3,8 @@
 var fs              = require('fs');
 var LocalStrategy   = require('passport-local').Strategy;
 var ldap            = require('ldapjs');
-var sql             = require("mssql");
+var sql             =
+    require("mssql");
 
 var dbConfig = {
     server: "PC106-4B\\SQLEXPRESS",
@@ -15,42 +16,50 @@ var dbConfig = {
 
 module.exports = function(passport) {
 
-    var users = [{"id":1, "username":"test", "password":"tester"}];
-
     // used to serialize the user for the session
     passport.serializeUser(function(user, done) {
-        done(null, users[0].id);
-        //done(null, user.id);
+        done(null, user.id);
+        console.log(user.username + " logged in");
     });
 
     // used to deserialize the user
     passport.deserializeUser(function(id, done) {
-        done(null, users[0]);
-        /*
         var conn = new sql.Connection(dbConfig);
         var request = new sql.Request(conn);
 
-        //var queryString = "SELECT person_Zagweb_ID from dbo.Person where dbo.Person.person_ID = " + id;
-        var queryString = "SELECT * from dbo.Person";
-        console.log(queryString);
-        request.query(queryString, function (err, username) {
+        conn.connect(function (err) {
             if (err) {
-                console.log(err);
-                console.log("Error: invalid query3");
+                console.log("Error: couldn't connect to the db");
+                return done(null, err);
             }
             else {
-                console.log(username[0]);
-                if(username[0] != null) {
-                    username[0].person_Zagweb_ID = username[0].person_Zagweb_ID.replace(/\s/g, '');
-                    done(null, username);
-                }
-                else {
-                    console.log("Error: user not in database");
-                }
+                var queryString = "SELECT person_Zagweb_ID from dbo.Person where dbo.Person.person_ID = " + id;
+                request.query(queryString, function (err, username) {
+                    if (err) {
+                        console.log("Error: invalid query");
+                        conn.close();
+                        return done(null, err);
+                    }
+                    else {
+                        if (username[0] != null) {
+                            username[0].person_Zagweb_ID = username[0].person_Zagweb_ID.replace(/\s/g, '');
+                            var user = {
+                                username : username[0].person_Zagweb_ID,
+                                id : id
+                            }
+                            conn.close();
+                            done(null, user);
+                            console.log(user.username + " made a request");
+                        }
+                        else {
+                            console.log("Error: user not in database");
+                            conn.close();
+                            return done(null, false);
+                        }
+                    }
+                });
             }
-            conn.close();
         });
-        */
     });
 
     // =====================================
@@ -79,55 +88,77 @@ module.exports = function(passport) {
             if (err) {
                 return done(null, false);
             }
-            else {
-                var conn = new sql.Connection(dbConfig);
-                var request = new sql.Request(conn);
 
-                conn.connect(function (err) {
+            var conn = new sql.Connection(dbConfig);
+            var request = new sql.Request(conn);
+            conn.connect(function (err) {
+                if (err) {
+                    console.log("Error: couldn't connect to the db");
+                    return done(null, false);
+                }
+                var queryString = "SELECT person_Zagweb_ID from dbo.Person where dbo.Person.person_Zagweb_ID = '" + username + "'";
+                request.query(queryString, function (err, name) {
                     if (err) {
-                        console.log("Error: couldn't connect to the db");
+                        console.log("Error: invalid query");
+                        conn.close();
                         return done(null, false);
                     }
-                    var queryString = "SELECT person_Zagweb_ID from dbo.Person where dbo.Person.person_Zagweb_ID = '" + username + "'";
-                    request.query(queryString, function (err, name) {
-                        if (err) {
-                            console.log("Error: invalid query1");
-                            return done(null, false);
-                        }
-                        else {
-                            if(name[0] != null) {
-                                queryString = "SELECT person_ID from dbo.Person where dbo.Person.person_Zagweb_ID = '" + username + "'";
-                                request.query(queryString, function (err, id) {
-                                    if (err) {
-                                        console.log("Error: invalid query2");
-                                        return done(null, false);
-                                    }
-                                    else {
-                                        if(id[0] != null) {
-                                            var user = {
-                                                username : username,
-                                                id : id[0].person_ID
-                                            }
-                                            conn.close();
-                                            return done(null, user);
-                                        }
-                                        else {
-                                            console.log("Error: user not in database");
-                                            return done(null, false);
-                                        }
-                                    }
-                                });
+                    else if(name[0] != null) {
+                        queryString = "SELECT person_ID from dbo.Person where dbo.Person.person_Zagweb_ID = '" + username + "'";
+                        request.query(queryString, function (err, id) {
+                            if (err) {
+                                console.log("Error: invalid query");
+                                conn.close();
+                                return done(null, false);
+                            }
+                            else if(id[0] != null) {
+                                var user = {
+                                    username : username,
+                                    id : id[0].person_ID
+                                }
+                                conn.close();
+                                return done(null, user);
                             }
                             else {
                                 console.log("Error: user not in database");
+                                conn.close();
                                 return done(null, false);
                             }
-                        }
-                        conn.close();
-                    });
+                        });
+                    }
+                    else {
+                        queryString = "INSERT INTO dbo.Person(person_Zagweb_ID) VALUES ('" + username + "')";
+                        request.query(queryString, function (err) {
+                            if (err) {
+                                console.log("Error: invalid insert new person query");
+                                conn.close();
+                                return done(null, false);
+                            }
+                        });
+                        queryString = "SELECT person_ID from dbo.Person where dbo.Person.person_Zagweb_ID = '" + username + "'";
+                        request.query(queryString, function (err, id) {
+                            if (err) {
+                                console.log("Error: invalid select new person query");
+                                conn.close();
+                                return done(null, false);
+                            }
+                            else if(id[0] != null) {
+                                var user = {
+                                    username : username,
+                                    id : id[0].person_ID
+                                }
+                                conn.close();
+                                return done(null, user);
+                            }
+                            else {
+                                console.log("Error: new user not in database");
+                                conn.close();
+                                return done(null, false);
+                            }
+                        });
+                    }
                 });
-            }
+            });
         });
     }));
-
 };
